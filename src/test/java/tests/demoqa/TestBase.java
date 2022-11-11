2a534c16504505cb725610a6e28271f165f01160
ArmanSakurov <a.sakurov@skyeng.ru>
2022-11-12 00:44:55 +0500

    [properties] added properties to run tests job with parameters

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -3,6 +3,7 @@ package tests.demoqa;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import tests.properties.Property;
 
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
@@ -16,15 +17,21 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("browserName", "chrome");
-        capabilities.setCapability("browserVersion", "100.0");
+//        capabilities.setCapability("browserName", "chrome");
+        Configuration.browser = Property.browser();
+        Configuration.browserVersion = Property.browserVersion();
+//        capabilities.setCapability("browserVersion", "100.0");
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = Property.browserSize();
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        if (!Property.remoteUrl().equals("")) {
+            Configuration.remote = Property.remoteUrl();
+        }
     }
 
     @AfterEach
