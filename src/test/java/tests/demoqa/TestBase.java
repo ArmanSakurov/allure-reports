8d503f5cf95e4da6a5e463605375ce1dbd33eb6c
ArmanSakurov <a.sakurov@skyeng.ru>
2022-11-13 20:04:54 +0500

    [properties] added fixes to build.gradle

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -12,23 +12,49 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
+//    @BeforeAll
+//    static void configure() {
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+//
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+////        capabilities.setCapability("browserName", "chrome");
+//        Configuration.browser = Property.browser();
+//        Configuration.browserVersion = Property.browserVersion();
+////        capabilities.setCapability("browserVersion", "100.0");
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", true);
+//
+//        Configuration.browserCapabilities = capabilities;
+//        Configuration.baseUrl = "https://demoqa.com";
+////        Configuration.browserSize = "1920x1080";
+//        Configuration.browserSize = Property.browserSize();
+////        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        if (!Property.remoteUrl().equals("")) {
+//            Configuration.remote = Property.remoteUrl();
+//        }
+//    }
+//
+//    @AfterEach
+//    void addAttachments() {
+//        Attach.screenshotAs("Last screenshot");
+//        Attach.pageSource();
+//        Attach.browserConsoleLogs();
+//        Attach.addVideo();
+//    }
+
     @BeforeAll
     static void configure() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("browserName", "chrome");
-        Configuration.browser = Property.browser();
-        Configuration.browserVersion = Property.browserVersion();
-//        capabilities.setCapability("browserVersion", "100.0");
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-//        Configuration.browserSize = "1920x1080";
+        Configuration.browser = Property.browser();
+        Configuration.browserVersion = Property.browserVersion();
         Configuration.browserSize = Property.browserSize();
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         if (!Property.remoteUrl().equals("")) {
             Configuration.remote = Property.remoteUrl();
         }
