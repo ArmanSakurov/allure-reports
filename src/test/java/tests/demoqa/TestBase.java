584762b7b325e99cf14cc796f9d6b346450e6fac
ArmanSakurov <a.sakurov@skyeng.ru>
2022-11-15 00:35:47 +0500

    [properties] added fixes to build.gradle

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -12,30 +12,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
-//    @BeforeAll
-//    static void configure() {
-//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-//
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("browserName", "chrome");
-//        capabilities.setCapability("browserVersion", "100.0");
-//        capabilities.setCapability("enableVNC", true);
-//        capabilities.setCapability("enableVideo", true);
-//
-//        Configuration.browserCapabilities = capabilities;
-//        Configuration.baseUrl = "https://demoqa.com";
-//        Configuration.browserSize = "1920x1080";
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-//    }
-//
-//    @AfterEach
-//    void addAttachments() {
-//        Attach.screenshotAs("Last screenshot");
-//        Attach.pageSource();
-//        Attach.browserConsoleLogs();
-//        Attach.addVideo();
-//    }
-
     @BeforeAll
     static void configure() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
